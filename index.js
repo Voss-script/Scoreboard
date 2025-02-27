72bda9ebe18e12d8c7a5e84f20e42c43dacf0986
Voss-script <nathanblasu09@gmail.com>
2025-02-27 20:00:43 +0100

    fix attempt

--- index.js
+++ index.js
@@ -4,40 +4,19 @@
  let count = 0
  let count2 = 0
  
- 
- function plus1(){
-    count += 1
-    home.textContent = count
-    console.log(count)
+
+ function increment(value){
+   
+   count += value
+   home.textContent = count
+   console.log(count)
  }
+
  
- function plus2(){
-    count += 2 
-    home.textContent = count
-    console.log(count)
- }
- 
- function plus3(){
-    count += 3
-    home.textContent = count
-    console.log(count)
- }
- 
- 
-  function plus1g(){
-     count2 += 1
+  function increment(value){
+     count2 += value
      guest.textContent = count2
      console.log(count2)
   }
  
-  function plus2g(){
-     count2 += 2
-     guest.textContent = count2
-     console.log(count2)
-  }
-  
-  function plus3g(){
-    count2 += 3
-    guest.textContent = count2
-    console.log(count2)
-  }
+ 
