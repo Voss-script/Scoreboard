85c44f9b4560cff8753054313cce43c82cb3ab19
Voss-script <nathanblasu09@gmail.com>
2025-02-27 18:50:36 +0100

    updated js file

--- index.js
+++ index.js
@@ -8,36 +8,36 @@
  function plus1(){
     count += 1
     home.textContent = count
-    // console.log(count)
+    console.log(count)
  }
  
  function plus2(){
     count += 2 
     home.textContent = count
-    // console.log(count)
+    console.log(count)
  }
  
  function plus3(){
     count += 3
     home.textContent = count
-    // console.log(count)
+    console.log(count)
  }
  
  
   function plus1g(){
      count2 += 1
      guest.textContent = count2
-    //  console.log(count2)
+     console.log(count2)
   }
  
   function plus2g(){
      count2 += 2
      guest.textContent = count2
-    //  console.log(count2)
+     console.log(count2)
   }
   
   function plus3g(){
     count2 += 3
     guest.textContent = count2
-    // console.log(count2)
+    console.log(count2)
   }
