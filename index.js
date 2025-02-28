5595484005f11c58a564003d4fc75433922aaa27
Voss-script <nathanblasu09@gmail.com>
2025-02-28 17:57:05 +0100

    if this doesnt work ill be moving on

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-let home = document.getElementById("homescore")
-let guest = document.getElementById("guestscore")
+const home = document.getElementById('homescore');
+const guest = document.getElementById('guestscore');
+const incrementBtn = document.getElementById('increment');
+const decrementBtn = document.getElementById('decrement');
 
-let count = 0
-let count2 = 0
+let count = 0;
+let count2 = 0;
 
-function increment(value){
-  count += value 
-  home.textContent = count
-  console.log(count)
+function increment(value) {
+    count += value;
+    home.textContent = count;
+    console.log
 }
 
-function increment2(value){
-  count2 += value
-  guest.textContent = count2
-  console.log(count2)
+function increment2(value) {
+    count2 += value;
+    guest.textContent = count2;
+    console.log(count2);
 }
+
+incrementBtn.addEventListener('click', increment);
